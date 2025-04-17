e3af0cb4352083f15896f9b9fe5f1996d541d097
jayad <jayadpathandev@gmail.com>
2025-04-18 00:00:05 +0530

    Console.Write() : use to print statement withought next line

--- syntax.cs
+++ syntax.cs
@@ -10,7 +10,12 @@ namespace hello
     {
         static void Main(string[] args)
         {
+            // Console.Write('String') : write statement withought end with line
+            Console.Write("hello world");
+
+            // Console.WriteLine('String') : write statement end with line
             Console.WriteLine("hello world");
+            
             Console.ReadLine();
            
         }
